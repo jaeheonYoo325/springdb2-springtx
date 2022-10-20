8911364aef7100ce00158dad6c193a19b5cbff25
jaeheonYoo325 <yoojh0325@gmail.com>
2022-10-20 09:00:15 +0900

    2022.10.20 : 트랜잭션 전파 활용3 - 단일 트랜잭션.

--- src/test/java/com/springdb2/springdb2springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/springdb2/springdb2springtx/propagation/MemberServiceTest.java
@@ -41,7 +41,7 @@ public class MemberServiceTest {
     /**
      * memberService        @Transactional:OFF
      * memberRepository     @Transactional:ON
-     * logRepository        @Transactional:ON EXCEPTION
+     * logRepository        @Transactional:ON Exception
      */
     @Test
     void outerTxOff_fail() {
@@ -55,5 +55,23 @@ public class MemberServiceTest {
         //then: 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService        @Transactional:ON
+     * memberRepository     @Transactional:OFF
+     * logRepository        @Transactional:OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
     }
 }
